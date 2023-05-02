48e6c631e942accde85faefad8997a1ae9d82998
haugy2349 <131355849+haugy2349@users.noreply.github.com>
2023-05-03 00:58:19 +0300

    Create locked.sol

---
+++ locked.sol
@@ -0,0 +1,34 @@
+pragma solidity ^0.8.0;
+
+contract TimeLockedSavingsAccount {
+   address payable public owner;
+   uint public unlockTime;
+   uint public balance;
+
+   constructor() payable {
+      owner = payable(msg.sender);
+      unlockTime = block.timestamp + 365 days; //Set to 1 year from now
+      balance = msg.value;
+   }
+
+   modifier onlyOwner() {
+      require(msg.sender == owner, "Only the owner can withdraw funds.");
+      _;
+   }
+
+   modifier onlyAfterUnlockTime() {
+      require(block.timestamp >= unlockTime, "Funds cannot be withdrawn yet.");
+      _;
+   }
+
+   function deposit() public payable {
+      balance += msg.value;
+   }
+
+   function withdraw() public onlyOwner onlyAfterUnlockTime {
+      owner.transfer(balance);
+      balance = 0;
+   }
+
+   }
+}
